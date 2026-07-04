8783dfc166c50dcdf52df7708622dbd26d50f7f8
Priyamvada Singh <Priyamvadasingh594@gmail.com>
2026-07-04 21:15:51 +0530

    fixed an error in user.py

--- backend/routers/user.py
+++ backend/routers/user.py
@@ -4,21 +4,27 @@ from fastapi import FastAPI,Response,status,HTTPException,Depends,APIRouter
 from sqlalchemy.orm import Session
 from ..database import  get_db
 from ..oauth2 import get_current_user
+from sqlalchemy.exc import IntegrityError
 router=APIRouter(
     prefix="/users",
     tags=['Users']
 
 )
+
+
 @router.post("/register",status_code=status.HTTP_201_CREATED,response_model=user.UserOut)
-def create_user(user: user.UserCreate, db: Session=Depends(get_db)):
-    existing=db.query(models.User).filter(models.User.email == user.email).first()
-    if existing:
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT,detail=f"user with email {user.email} already exists.")
-    hashed_password=utils.hash(user.password)
-    user.password=hashed_password
-    new_user=models.User(**user.dict())
+def create_user(user: user.UserCreate, db: Session = Depends(get_db)):
+    hashed_password = utils.hash(user.password)
+    new_user = models.User(**user.dict(exclude={"password"}), password=hashed_password)
     db.add(new_user)
-    db.commit()
+    try:
+        db.commit()
+    except IntegrityError:
+        db.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="This email or phone number is already registered."
+        )
     db.refresh(new_user)
     return new_user
 
